0b6bc3ee8f26fe4432fd451420885bb2fdbaec4b
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-03-13 03:03:36 +0530

    Payload now uses the jump-call-pop trick to load address of string in the register.

--- testing/hello.asm
+++ testing/hello.asm
@@ -3,12 +3,15 @@
         section .text
 _start: mov     rax, 1          ; syscall number for write-syscall
         mov     rdi, 1          ; first argument for write-syscall: stdout
-        mov     rsi, hw_str     ; second argument for write-syscall: address of string
+        jmp _trick
+_continue:
+        pop     rsi             ; Address of the string is now in rsi register
         mov     rdx, 13         ; third argument for write-syscall: number of bytes to write
         syscall                 ; the write-syscall
         mov     rax, 60         ; syscall number for exit
-        xor     rdi, rdi          ; first argument for exit: exit-code
+        xor     rdi, rdi        ; first argument for exit: exit-code
         syscall                 ; the exit-syscall
 
-        section .data
-hw_str: db      "Hello World!", 10
+_trick:
+        call _continue
+        db      "Hello World!!", 10
