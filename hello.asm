a5b9a52c48fbd97c6cfcb6b4c0fec13321789c53
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-03-17 19:23:42 +0530

    cleaned files for submission

--- hello.asm
+++ hello.asm
@@ -1,19 +1,19 @@
         global _start
         section .text
 _start: xor     rax, rax
-        mov     al, 0x01          ; syscall number for write-syscall
+        mov     al, 0x01                ; syscall number for write-syscall
         xor     rdi, rdi
-        mov     dil, 0x01          ; first argument for write-syscall: stdout
+        mov     dil, 0x01               ; first argument for write-syscall: stdout
         jmp _trick
 _continue:
-        pop     rsi             ; Address of the string is now in rsi register
+        pop     rsi                     ; Address of the string is now in rsi register
         xor     rdx, rdx
-        mov     dl, 0x0d         ; third argument for write-syscall: number of bytes to write; change to 14
-        syscall                 ; the write-syscall
+        mov     dl, 0x0d                ; third argument for write-syscall: number of bytes to write; change to 14
+        syscall                         ; the write-syscall
         xor     rax, rax
-        mov     al, 0x3c         ; syscall number for exit
-        xor     rdi, rdi        ; first argument for exit: exit-code
-        syscall                 ; the exit-syscall
+        mov     al, 0x3c                ; syscall number for exit
+        xor     rdi, rdi                ; first argument for exit: exit-code
+        syscall                         ; the exit-syscall
 _trick:
         call _continue
-        db      "Hello world!", 0x20 ; why are three "!"s not working?
+        db      "Hello world!", 0x20
